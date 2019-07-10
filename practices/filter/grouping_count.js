bd29402bd0a031afe28f20b0d8703968aadb99b4
alauhy <751961605@qq.com>
2019-07-10 15:13:32 +0800

    finished grouping_count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,7 +1,25 @@
 'use strict';
 
 function grouping_count(collection) {
-
+  let collection_arr=[];
+   collection.forEach(number => {
+     if(collection_arr.indexOf(number)==-1){
+       collection_arr.push(number);
+     }
+     
+   });
+   let result=new Object();
+   collection_arr.forEach(number => {
+       let cnt=0;
+       collection.forEach(i=>{
+         if(i==number){
+           cnt++;
+         }
+       })
+       result[number]=cnt;
+     
+   });
+   return result;
   //implement here
 }
 
